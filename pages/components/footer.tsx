7fc92fc811eed0bc0e6fcb5b0ff07fc066cb764b
Rippley777 <ally.rippley@gmail.com>
2022-10-21 14:25:53 -0500

    fix footer

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -3,8 +3,10 @@ import { NextComponentType } from "next";
 const Footer: NextComponentType = () => {
   return (
     <footer className="flex flex-1 my-auto justify-center text-gray-600">
-      <p>Ally Rippley 2022</p>
-      <p>build using next.js, react, javascript and typescript</p>
+      <div>
+        <p>Ally Rippley 2022</p>
+        <p>Built using next.js, react, javascript and typescript</p>
+      </div>
     </footer>
   );
 };
